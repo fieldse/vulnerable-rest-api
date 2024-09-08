7f116e3252262fdbcb8fa3b71f2251415ecbdfa0
Matt <mattfields+dev@pm.me>
2024-09-08 11:52:25 +1000

    cleanup

--- index.js
+++ index.js
@@ -8,18 +8,11 @@ const { PORT, API_VERSION } = config;
 
 // GET /
 router.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World!');
 });
 
 // Set base URL to /api/v1
 app.use(`/api/${API_VERSION}`, router);
-
-// Debug
-const debugParams = {
-  PORT,
-  API_VERSION,
-};
-console.log('=== debug: ', JSON.stringify(debugParams, null, 2));
 app.listen(PORT, () => {
   console.log(`API started on ${PORT}`);
 });
