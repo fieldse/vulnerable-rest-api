3bf7cd63436e1595433d75e799be7b477faf1872
Matt <394065+fieldse@users.noreply.github.com>
2024-09-17 15:41:40 +1000

    add working contact routes (#5)

--- src/queries.js
+++ src/queries.js
@@ -2,9 +2,9 @@
 
 import db from './dbConn.js';
 
-// Create a new connection and perform a query.
-// Does not accept params, because we're aiming for SQL injection by forcing the user to interpolate strings.
-export async function query(sql) {
+// Create a new connection and perform an unsafe query.
+// Does not accept query params, because we're aiming for SQL injection by forcing the user to interpolate strings.
+export async function queryUnsafe(sql) {
   const dbConn = await db.newConn();
   try {
     const [result] = await dbConn.query(sql);
@@ -28,7 +28,7 @@ export async function querySafe(sql, ...params) {
 
 // Validate password -- returns boolean based on password match.
 export async function validatePassword(email, password) {
-  const rows = await query(
+  const rows = await queryUnsafe(
     `SELECT password FROM users WHERE email ='${email}'`
   );
   if (!rows) {
@@ -39,7 +39,7 @@ export async function validatePassword(email, password) {
 
 // Update Password by ID
 export async function updatePasswordById(id, newPassword) {
-  return await query(
+  return await queryUnsafe(
     `UPDATE users SET password = '${newPassword}' where id = ${id} `
   );
 }
@@ -49,19 +49,19 @@ export async function updatePasswordByEmail(email, newPassword) {
   if (!email || !newPassword) {
     throw new Error('email and password parameters are required');
   }
-  return await query(
+  return await queryUnsafe(
     `UPDATE users SET password = '${newPassword}' where email = '${email}' `
   );
 }
 
 // Get all users
 export async function getUsers() {
-  return await query('select id, name, email, role from users');
+  return await queryUnsafe('SELECT id, name, email, role FROM users');
 }
 
 // Get single user by ID
 export async function getUserByID(id) {
-  const result = await query(
+  const result = await queryUnsafe(
     `SELECT id, name, email, role FROM users WHERE id = ${id}` // unsafe string interpolation ; vulnerable to SQL injection
   );
   return result[0];
@@ -80,13 +80,13 @@ export async function addUser(name, email, role) {
 // Get single user by email
 export async function getUserByEmail(email) {
   const q = `SELECT id, name, email, role FROM users WHERE email = '${email}'`; // unsafe string interpolation ; vulnerable to SQL injection
-  const rows = await query(q);
+  const rows = await queryUnsafe(q);
   return rows[0];
 }
 
 // Update user. Returns true if rows matched.
 export async function updateUser(id, name, email) {
-  const result = await query(
+  const result = await queryUnsafe(
     `UPDATE users SET name = '${name}', email = '${email}' WHERE id = ${id}` // unsafe string interpolation ; vulnerable to SQL injection
   );
   return !!result.affectedRows;
@@ -99,7 +99,7 @@ export async function deleteUser(id) {
 
 // Get all news
 export async function getNews() {
-  return await query(
+  return await queryUnsafe(
     'SELECT n.id, n.title, n.content, u.email, u.name AS user_name FROM news n LEFT JOIN users u ON n.posted_by_id = u.id'
   );
 }
@@ -122,7 +122,7 @@ export async function deleteNews(id) {
 
 // Get all message board entries
 export async function getMessages() {
-  return await query(
+  return await queryUnsafe(
     'SELECT m.id, m.title, m.content, u.email, u.name AS user_name FROM message_board m LEFT JOIN users u ON m.posted_by_id = u.id'
   );
 }
@@ -143,6 +143,23 @@ export async function deleteMessage(id) {
   return await querySafe('DELETE FROM message_board WHERE id = ?', id);
 }
 
+// Get contact form entries
+export async function getContacts() {
+  return await queryUnsafe(`SELECT * FROM contact`);
+}
+
+// Add contact table item (for contact form). Returns ID on success
+export async function addContact(name, email, message) {
+  const result = await queryUnsafe(
+    `INSERT INTO contact (name, email, message) VALUES ( '${name}', '${email}', '${message}')` // dramatically insecure for a contact form
+  );
+  return result.insertId;
+}
+// Delete contact entry
+export async function deleteContact(id) {
+  return await querySafe('DELETE FROM contact WHERE id = ?', id);
+}
+
 export default {
   getUsers,
   getUserByID,
@@ -158,4 +175,7 @@ export default {
   getMessages,
   addMessage,
   deleteMessage,
+  getContacts,
+  addContact,
+  deleteContact,
 };
